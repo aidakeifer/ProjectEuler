a5b799059ed8224de69868d3e844d1167a25510e
jkeifer <jarrettkeifer@gmail.com>
2021-03-12 22:23:41 -0800

    solved 0007 in racket

---
+++ 0007/racket/10001_prime.rkt
@@ -0,0 +1,31 @@
+#lang racket
+(define (is_prime num)
+  (define (_is_prime num divisor)
+    (let-values ([(q r) (quotient/remainder num divisor)])
+      (if (eq? r 0)
+        (if (eq? q 1)
+            #t
+            #f
+        )
+        (_is_prime num (+ divisor 1))
+      )
+    )
+  )
+  (_is_prime num 2)
+)
+
+
+(define (nth_prime n)
+  (define (_nth_prime n num)
+    (when (is_prime num)
+      (set! n (- n 1))
+    )
+    (if (<= n 0)
+      num
+      (_nth_prime n (+ num 1))
+    )
+  )
+  (_nth_prime n 2)
+)
+
+(nth_prime 10001)
