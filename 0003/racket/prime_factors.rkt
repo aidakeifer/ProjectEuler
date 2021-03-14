603933486ebe3acf0fceaa604ba06780196fda39
jkeifer <jarrettkeifer@gmail.com>
2021-03-13 16:13:16 -0800

    0003: redo racket version as cli prog

--- 0003/racket/prime_factors.rkt
+++ 0003/racket/prime_factors.rkt
@@ -1,4 +1,6 @@
+#!/usr/bin/env racket
 #lang racket
+
 (define (last l)
   (cond ((null? (cdr l)) (car l))
         (else (last (cdr l)))))
@@ -9,7 +11,7 @@
     (let-values ([(q r) (quotient/remainder num divisor)])
       (when (eq? r 0)
         (set! num q)
-        (set! factors (append factors (list divisor)))
+        (set! factors (append factors (list (number->string divisor))))
         (set! divisor 1)
       )
       (if (eq? num 1)
@@ -22,4 +24,26 @@
 )
 
 
-(last (factor  600851475143))
+(define all-factors (make-parameter #f))
+
+(define number-to-factor
+  (command-line
+    #:program "Prime factorizer"
+    #:once-each
+    [("-a" "--all") "List all factors, not just largest"
+        (all-factors #t)]
+    #:args (number)
+    (string->number number)
+  )
+)
+
+
+(define (do_factoring)
+  (define factors (factor  number-to-factor))
+  (if (all-factors)
+    (string-join factors ", " #:after-last "")
+    (last factors)
+  )
+)
+
+(displayln (do_factoring))
